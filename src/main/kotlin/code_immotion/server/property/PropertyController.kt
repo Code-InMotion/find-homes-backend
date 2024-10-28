71c84b70cafd68036a6dd04ba4c5deca0514b776
klaus9267 <klaus9267@gmail.com>
2024-10-29 00:09:07 +0900

    Feat: 개발용 매물 전체 삭제, 전체 조회 기능 구현

--- src/main/kotlin/code_immotion/server/property/PropertyController.kt
+++ src/main/kotlin/code_immotion/server/property/PropertyController.kt
@@ -1,11 +1,16 @@
 package code_immotion.server.property
 
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("property")
-class PropertyController(
-    private val propertyService: PropertyService
-) {
+class PropertyController(private val propertyService: PropertyService) {
+    @GetMapping
+    fun readAll() = propertyService.readAll()
+
+    @DeleteMapping
+    fun deleteAll() = propertyService.deleteAll()
 }
--- src/main/kotlin/code_immotion/server/property/PropertyService.kt
+++ src/main/kotlin/code_immotion/server/property/PropertyService.kt
@@ -1,9 +1,11 @@
 package code_immotion.server.property
 
+import code_immotion.server.property.entity.Property
 import org.springframework.stereotype.Service
 
 @Service
-class PropertyService(
-    private val propertyRepository: PropertyRepository,
-) {
+class PropertyService(private val propertyRepository: PropertyRepository) {
+    fun readAll(): List<Property> = propertyRepository.findAll()
+
+    fun deleteAll() = propertyRepository.deleteAll()
 }
