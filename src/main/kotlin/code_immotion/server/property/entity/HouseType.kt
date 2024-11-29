f59686c5dbddac92d0f53e3bee3796d2d211d27e
Kim minho <klaus9267@gmail.com>
2024-11-29 23:35:48 +0900

    Refactor: 거주 유형 수정

--- src/main/kotlin/code_immotion/server/property/entity/HouseType.kt
+++ src/main/kotlin/code_immotion/server/property/entity/HouseType.kt
@@ -3,9 +3,5 @@ package code_immotion.server.property.entity
 enum class HouseType {
     APARTMENT, // 아파트
     OFFICETEL, // 오피스텔
-    // SINGLE_FAMILY,
-    // MULTI_FAMILY,
-    // 단독/다가구는 지번 주스 미제공
-    TOWNHOUSE, // 연립
-    MULTI_UNIT // 다세대
+    VILLA // 빌라
 }
