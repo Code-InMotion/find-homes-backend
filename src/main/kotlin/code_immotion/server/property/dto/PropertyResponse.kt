1a765b3e00a911663b802f2804bbf4fd242026e5
Kim minho <klaus9267@gmail.com>
2024-11-29 23:36:02 +0900

    Feat: 매물 목록 반환에 거래 유형 추가

--- src/main/kotlin/code_immotion/server/property/dto/PropertyResponse.kt
+++ src/main/kotlin/code_immotion/server/property/dto/PropertyResponse.kt
@@ -2,10 +2,12 @@ package code_immotion.server.property.dto
 
 import code_immotion.server.property.entity.HouseType
 import code_immotion.server.property.entity.Property
+import code_immotion.server.property.entity.TradeType
 import org.springframework.data.geo.Distance
 
 data class PropertyResponse(
     val id: String,
+    val type: TradeType,
     val price: Long,
     val rentPrice: Long?,
     val address: String,
@@ -19,6 +21,7 @@ data class PropertyResponse(
         fun from(property: Property, distance: Distance): PropertyResponse {
             return PropertyResponse(
                 id = property.id!!,
+                type = property.type,
                 price = property.price,
                 rentPrice = property.rentPrint,
                 address = property.address,
