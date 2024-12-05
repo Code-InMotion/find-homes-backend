7246b1ca385c729a03b34dcb0392108c61ca39a8
Kim minho <klaus9267@gmail.com>
2024-12-05 19:53:41 +0900

    Docs: dto field 설명 작성

--- src/main/kotlin/code_immotion/server/property/dto/PropertyCondition.kt
+++ src/main/kotlin/code_immotion/server/property/dto/PropertyCondition.kt
@@ -2,34 +2,34 @@ package code_immotion.server.property.dto
 
 import code_immotion.server.property.entity.HouseType
 import code_immotion.server.property.entity.TradeType
-import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Schema
 
-class PropertyCondition(
-    @Parameter(example = "0", required = false, description = "매매 / 보증금 최소금액")
+data class PropertyCondition(
+    @field:Schema(description = "매매 / 보증금 최소금액", example = "1_000_000L")
     val minPrice: Long = 0,
 
-    @Parameter(example = "10_000_000_000L", required = false, description = "매매 / 보증금 최고금액")
+    @field:Schema(description = "매매 / 보증금 최고금액", example = "10_000_000_000L")
     val maxPrice: Long = 10_000_000_000L,
 
-    @Parameter(example = "0", required = false, description = "월세 최소금액")
+    @field:Schema(example = "0", required = false, description = "월세 최소금액")
     val minRentPrice: Long = 0,
 
-    @Parameter(example = "100_000_000L", required = false, description = "월세 최고금액")
+    @field:Schema(example = "100_000_000L", required = false, description = "월세 최고금액")
     val maxRentPrice: Long = 100_000_000L,
 
-    @Parameter(required = false, description = "정렬 기준")
-    val sortType: SortType,
+    @field:Schema(required = false, description = "정렬 기준")
+    val sortType: SortType = SortType.PRICE,
 
-    @Parameter(required = false, description = "이동 소요 시간(분 기준)")
+    @field:Schema(required = false, description = "이동 소요 시간(분 기준)")
     val travelTime: Int,
 
-    @Parameter(example = "경기도 성남시 분당구 정지일로 95", required = true, description = "목적지(회사/학교 등)")
+    @field:Schema(example = "경기도 성남시 분당구 정지일로 95", required = true, description = "목적지(회사/학교 등)")
     val destination: String,
 
-    @Parameter(required = false, description = "주거 형태")
+    @field:Schema(required = false, description = "주거 형태")
     val houseType: List<HouseType> = HouseType.entries.toList(),
 
-    @Parameter(required = false, description = "거래 형태")
+    @field:Schema(required = false, description = "거래 형태")
     val tradeType: List<TradeType> = TradeType.entries.toList()
 ) {
 
