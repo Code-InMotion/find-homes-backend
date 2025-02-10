028b09946618f1d320ffd828d125f287da113812
Kim minho <klaus9267@gmail.com>
2025-02-10 14:43:33 +0900

    Feat: id를 복합키로 변경

--- src/main/kotlin/code_immotion/server/domain/route/entity/StopTime.kt
+++ src/main/kotlin/code_immotion/server/domain/route/entity/StopTime.kt
@@ -1,30 +1,28 @@
 package code_immotion.server.domain.route.entity
 
 import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
+import jakarta.persistence.IdClass
 import jakarta.persistence.ManyToOne
-import org.springframework.data.domain.Persistable
 import java.time.LocalTime
 
 
 @Entity
 class StopTime(
     @Id
-    @GeneratedValue
-    val id: Long,
     @ManyToOne
     val trip: Trip,
+
+    @Id
     @ManyToOne
     val stop: Stop,
+
+    @Id
+    val stopSequence: Int,
+
     val arrivalTime: LocalTime,
     val departureTime: LocalTime,
-    val stopSequence: Int,
     val pickupType: Int, // 0: 승차 가능, 1: 승차 불가
     val dropOffType: Int, // 0: 하차 가능, 1: 하차 불가
     val timePoint: Int  // 1: 정확한 시각
-) : Persistable<Long> {
-    override fun getId() = id
-
-    override fun isNew() = true
-}
+)
